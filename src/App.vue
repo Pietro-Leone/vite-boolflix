2c2e52fc1274d1342cb5dc4d3f9da64cc4df4a4b
Pietro-Leone <leonepietro1999@gmail.com>
2023-08-28 13:29:07 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import Header from "./components/TheHeader.vue"
+import Main from "./components/TheMain.vue"
+export default {
+  components: {
+    Header,
+    Main,
+  }
+}
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss"></style>
